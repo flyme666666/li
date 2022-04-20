85df7ad431555d2582c2259e4df477546b87185f
lyw <1421379266@qq.com>
2022-04-20 21:02:26 +0800

    leetCode-2022-04-20

--- src/leetcode/y2022/leet0420/Solution.java
+++ src/leetcode/y2022/leet0420/Solution.java
@@ -1,4 +1,50 @@
 package leetcode.y2022.leet0420;
 
+/**
+ *     作者：LeetCode-Solution
+ *     链接：https://leetcode-cn.com/problems/longest-absolute-file-path/solution/wen-jian-de-zui-chang-jue-dui-lu-jing-by-fi0r/
+ *     来源：力扣（LeetCode）
+ *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
 public class Solution {
+
+    public int lengthLongestPath(String input) {
+        int n = input.length();
+        int pos = 0;
+        int ans = 0;
+        int[] level = new int[n + 1];
+
+        while (pos < n) {
+            /* 检测当前文件的深度 */
+            int depth = 1;
+            while (pos < n && input.charAt(pos) == '\t') {
+                pos++;
+                depth++;
+            }
+            /* 统计当前文件名的长度 */
+            int len = 0;
+            boolean isFile = false;
+            while (pos < n && input.charAt(pos) != '\n') {
+                if (input.charAt(pos) == '.') {
+                    isFile = true;
+                }
+                len++;
+                pos++;
+            }
+            /* 跳过换行符 */
+            pos++;
+
+            if (depth > 1) {
+                len += level[depth - 1] + 1;
+            }
+            if (isFile) {
+                ans = Math.max(ans, len);
+            } else {
+                level[depth] = len;
+            }
+        }
+        return ans;
+    }
+
+
 }
