043a31ce4729fe282aca7d70e3de371d697cea35
ywli <ywli@hillinsight.com>
2022-04-25 14:32:18 +0800

    2022-04-25

---
+++ src/leetcode/y2022/leet0425/Solution.java
@@ -0,0 +1,52 @@
+package leetcode.y2022.leet0425;
+
+import java.util.*;
+
+/**
+ * 给定一个可能含有重复元素的整数数组，要求随机输出给定的数字的索引。 您可以假设给定的数字一定存在于数组中。
+ *
+ * 注意：
+ * 数组大小可能非常大。 使用太多额外空间的解决方案将不会通过测试。
+ *
+ * int[] nums = new int[] {1,2,3,3,3};
+ * Solution solution = new Solution(nums);
+ *
+ * // pick(3) 应该返回索引 2,3 或者 4。每个索引的返回概率应该相等。
+ * solution.pick(3);
+ *
+ * // pick(1) 应该返回 0。因为只有nums[0]等于1。
+ * solution.pick(1);
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/random-pick-index
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * 作者：LeetCode-Solution
+ *         链接：https://leetcode-cn.com/problems/random-pick-index/solution/sui-ji-shu-suo-yin-by-leetcode-solution-ofsq/
+ *         来源：力扣（LeetCode）
+ *         著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class Solution {
+
+    Map<Integer, List<Integer>> pos;
+    Random random;
+
+    public Solution(int[] nums) {
+        pos = new HashMap<Integer, List<Integer>>();
+        random = new Random();
+        for (int i = 0; i < nums.length; ++i) {
+            pos.putIfAbsent(nums[i], new ArrayList<Integer>());
+            pos.get(nums[i]).add(i);
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> indices = pos.get(target);
+        return indices.get(random.nextInt(indices.size()));
+    }
+
+}
+
+
+
