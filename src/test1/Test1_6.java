256a2f4bf3c15f53a879f5205b4795ab97547bd7
liyingwen <1421379266@qq.com>
2023-04-04 18:21:02 +0800

    2023-04-04

--- src/test1/Test1_6.java
+++ src/test1/Test1_6.java
@@ -1,5 +1,8 @@
 package test1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Test1_6 {
 
 
@@ -14,12 +17,28 @@ public class Test1_6 {
 //
 //        System.out.println(b);
 
-        Integer A = 10000, B = 100 , C = 100;
-        System.out.println(A==B); //false
+//        Integer A = 10000, B = 100 , C = 100;
+//        System.out.println(A==B); //false
+//
+//        System.out.println(B==C); //true
 
-        System.out.println(B==C); //true
+        sort();
 
+    }
 
+    //冒泡排序
+    public static void sort(){
+        int [] arr =  {1,3,44,5,676,4};
+        for (int i = arr.length; i > 0 ; i--) {
+            for (int j = 0 ; j < i-1 ; j++){
+                if(arr[j] > arr [j+1]){
+                    int temp = arr [j+1];
+                    arr [j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 
 
